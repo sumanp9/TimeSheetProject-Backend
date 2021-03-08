0d7110d6f70de8ed82f2d1bf0ad4787b8978601e
Suman Pradhab <11.sumanpradhan@gmail.com>
2021-03-08 10:08:27 -0600

    first commit for timesheet backend

---
+++ src/main/java/com/tms/portal/timesheetportal/TimesheetportalApplication.java
@@ -0,0 +1,13 @@
+package com.tms.portal.timesheetportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimesheetportalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimesheetportalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tms/portal/timesheetportal/TimesheetportalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tms.portal.timesheetportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimesheetportalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
